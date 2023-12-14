2392bf693751ab7b97bb6f8d83a4b645b5bc21b1
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2023-12-14 23:03:05 +0530

    email verification added

--- ecommerce/ecomauth/views.py
+++ ecommerce/ecomauth/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
+from django.views.generic import View
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+#activate user account
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
+from django.urls import NoReverseMatch,reverse
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes,DjangoUnicodeDecodeError
+# from django.utils.encoding import force_text
+from django.utils.encoding import force_str
+
+#Geting Tokens from Utils
+from .utils import generate_token
+from .utils import TokenGenerator
+
+#emails
+from django.core.mail import send_mail,EmailMultiAlternatives,EmailMessage
+from django.core.mail import BadHeaderError,send_mail
+from django.core import mail
+from django.conf import settings
+from django.core.mail import EmailMessage
+#threading
+import threading
+
+class EmailThread(threading.Thread):
+    def __init__(self, email_message):
+        self.email_message = email_message
+        # Threading.Thread.__init__(self)
+        threading.Thread.__init__(self)
+    def run(self):
+        self.email_message.send()
 
 # Create your views here.
 def signup(request):
@@ -22,12 +52,46 @@ def signup(request):
             pass
         
         myuser = User.objects.create_user(email,email,password)
+        # myuser.is_active=False
         myuser.save()
-        messages.info(request,"Signup SuccessFull! Please Login")
+        # current_site = get_current_site(request)
+        # #activate account
+        # email_subject = "Activate your Account"
+        # message = render_to_string('auth/activate.html',{
+        #     'user': myuser,
+        #     'domain': '127.0.0.1:8000',
+        #     'uid':urlsafe_base64_encode(force_bytes(myuser.pk)),
+        #     'token': generate_token.make_token(myuser)
+        # })
+        # #sent a message
+        # email_message = EmailMessage(email_subject,message,settings.EMAIL_HOST_USER,[email],)
+        # EmailThread(email_message).start()
+        # messages.info(request,"Activate Your Account by clicking link on your email")
+        messages.info(request,"Registeration Successfully")
         return redirect('/ecomauth/login')
     
     return render(request, 'auth/signup.html')
 
+
+class ActivateAccountView(View):
+    def get(self, request,uidb64,token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except Exception as identifier:
+            user=None
+        
+        if user is not None and generate_token.check_token(user,token):
+            user.is_active=True
+            user.save()
+            messages.info(request,"Account Activated Successfully")
+            return redirect('/ecomauth/login')
+        
+        return render(request, 'auth/activate_fail.html')
+            
+            
+            
+    
 
 def handlelogin(request):
     if request.method=='POST':
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -133,3 +133,9 @@ MESSAGE_TAGS = {
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER = 'bagbody672@gmail.com'
+EMAIL_HOST_PASSWORD = 'l@dyment0r'
+EMAIL_PORT = 587
+EMAIL_USE_TLS=True
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
