6dcf2f7428e7ab75e7d6e41412ef7638736e7426
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2023-12-22 00:33:12 +0530

    created two tables for orders and track orders

--- ecommerce/ecomapp/urls.py
+++ ecommerce/ecomapp/urls.py
@@ -4,5 +4,6 @@ from  . import views
 
 urlpatterns = [
     path('',views.home, name = 'home'),
-    path('purchase',views.Purchase, name = "purchase")
+    path('purchase',views.Purchase, name = "purchase"),
+    path('checkout/',views.checkout, name = "checkout"),
 ]
--- ecommerce/ecomapp/views.py
+++ ecommerce/ecomapp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from . models import Product
+from django.shortcuts import render, redirect
+from . models import Product,Orders,OrderUpdate
 from math import ceil
 from ecomapp import key
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request,'index.html')
@@ -22,4 +23,40 @@ def Purchase(request):
     params = {'allProds': allProds}
     return render(request,'purchase.html', params)
 
-
+def checkout(request):
+    if not request.user.is_authenticated:
+        messages.warning(request,"Login & Again")
+        return redirect('/ecomauth/login')
+    
+    if request.method=="POST":
+        
+        itemn_json = request.POST.get('itemsJson','')
+        name = request.POST.get('name','')
+        amount = request.POST.get('amt')
+        email = request.POST.get('email','')
+        address1 = request.POST.get('address1','')
+        address2  = request.POST.get('address2','')
+        city = request.POST.get('city','')
+        state = request.POST.get('state','')
+        zip_code = request.POST.get('zip_code','')  
+        phone = request.POST.get('phone','') 
+        
+        
+        order = Orders(itemn_json=itemn_json, name=name, amount=amount,email=email,address1=address1,address2=address2,
+                       city=city,state=state,zip_code=zip_code,
+                       phone=phone) 
+        print(amount)
+        order.save()
+        update = OrderUpdate(order_id = order.order_id,update_desc="The order has been placed")
+        update.save()
+        thank = True
+        
+        #payment intigration
+        id = order.order_id
+        oid = str(id)+"bodybag"
+        param_dict = {
+            'MID': 'MID',
+            'ORDER'
+            
+        }
+    return render(request, 'checkout.html')   
